2f86b3e45da680ed68eed4cbafaa9c12b7598939
John Davidson <jwdavidson@gmail.com>
2015-03-03 14:38:25 -0500

    Added Owner Trust dialog

---
+++ LynxPrivacy/LocalKeyStoreDialogs/OwnerTrust.Designer.cs
@@ -0,0 +1,303 @@
+﻿namespace LynxPrivacy.LocalKeyStoreDialogs
+{
+    partial class OwnerTrust
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtFingerprint = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.chkUltimateTrust = new System.Windows.Forms.CheckBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.btnSetTrustLevel = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.rdoKnownProblem = new System.Windows.Forms.RadioButton();
+            this.rdoNoTrust = new System.Windows.Forms.RadioButton();
+            this.rdoMinimalTrust = new System.Windows.Forms.RadioButton();
+            this.rdoFullTrust = new System.Windows.Forms.RadioButton();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtFingerprint
+            // 
+            this.txtFingerprint.Location = new System.Drawing.Point(132, 62);
+            this.txtFingerprint.Name = "txtFingerprint";
+            this.txtFingerprint.ReadOnly = true;
+            this.txtFingerprint.Size = new System.Drawing.Size(327, 20);
+            this.txtFingerprint.TabIndex = 55;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(132, 36);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.ReadOnly = true;
+            this.txtEmail.Size = new System.Drawing.Size(327, 20);
+            this.txtEmail.TabIndex = 54;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(132, 12);
+            this.txtName.Name = "txtName";
+            this.txtName.ReadOnly = true;
+            this.txtName.Size = new System.Drawing.Size(327, 20);
+            this.txtName.TabIndex = 53;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(39, 68);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 13);
+            this.label3.TabIndex = 52;
+            this.label3.Text = "Fingerprint";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(39, 39);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(33, 13);
+            this.label2.TabIndex = 51;
+            this.label2.Text = "EMail";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(39, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 50;
+            this.label1.Text = "Name";
+            // 
+            // chkUltimateTrust
+            // 
+            this.chkUltimateTrust.AutoSize = true;
+            this.chkUltimateTrust.Location = new System.Drawing.Point(42, 281);
+            this.chkUltimateTrust.Name = "chkUltimateTrust";
+            this.chkUltimateTrust.Size = new System.Drawing.Size(91, 17);
+            this.chkUltimateTrust.TabIndex = 60;
+            this.chkUltimateTrust.Text = "Ultimate Trust";
+            this.chkUltimateTrust.UseVisualStyleBackColor = true;
+            this.chkUltimateTrust.CheckedChanged += new System.EventHandler(this.chkUltimateTrust_CheckedChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(158, 122);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 61;
+            this.label4.Text = "label4";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(158, 168);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.TabIndex = 62;
+            this.label5.Text = "label5";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(158, 205);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(35, 13);
+            this.label6.TabIndex = 63;
+            this.label6.Text = "label6";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(158, 238);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(35, 13);
+            this.label7.TabIndex = 64;
+            this.label7.Text = "label7";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(158, 282);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(35, 13);
+            this.label8.TabIndex = 65;
+            this.label8.Text = "label8";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(75, 325);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(35, 13);
+            this.label9.TabIndex = 66;
+            this.label9.Text = "label9";
+            // 
+            // btnSetTrustLevel
+            // 
+            this.btnSetTrustLevel.Location = new System.Drawing.Point(347, 319);
+            this.btnSetTrustLevel.Name = "btnSetTrustLevel";
+            this.btnSetTrustLevel.Size = new System.Drawing.Size(99, 24);
+            this.btnSetTrustLevel.TabIndex = 67;
+            this.btnSetTrustLevel.Text = "Set Trust Level";
+            this.btnSetTrustLevel.UseVisualStyleBackColor = true;
+            this.btnSetTrustLevel.Click += new System.EventHandler(this.btnSetTrustLevel_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnCancel.Location = new System.Drawing.Point(480, 319);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(90, 24);
+            this.btnCancel.TabIndex = 68;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // rdoKnownProblem
+            // 
+            this.rdoKnownProblem.AutoSize = true;
+            this.rdoKnownProblem.Location = new System.Drawing.Point(19, 12);
+            this.rdoKnownProblem.Name = "rdoKnownProblem";
+            this.rdoKnownProblem.Size = new System.Drawing.Size(81, 17);
+            this.rdoKnownProblem.TabIndex = 69;
+            this.rdoKnownProblem.TabStop = true;
+            this.rdoKnownProblem.Text = "Never Trust";
+            this.rdoKnownProblem.UseVisualStyleBackColor = true;
+            this.rdoKnownProblem.Click += new System.EventHandler(this.rdoKnownProblem_Click);
+            // 
+            // rdoNoTrust
+            // 
+            this.rdoNoTrust.AutoSize = true;
+            this.rdoNoTrust.Location = new System.Drawing.Point(19, 58);
+            this.rdoNoTrust.Name = "rdoNoTrust";
+            this.rdoNoTrust.Size = new System.Drawing.Size(98, 17);
+            this.rdoNoTrust.TabIndex = 70;
+            this.rdoNoTrust.TabStop = true;
+            this.rdoNoTrust.Text = "Unknown Trust";
+            this.rdoNoTrust.UseVisualStyleBackColor = true;
+            this.rdoNoTrust.Click += new System.EventHandler(this.rdoNoTrust_Click);
+            // 
+            // rdoMinimalTrust
+            // 
+            this.rdoMinimalTrust.AutoSize = true;
+            this.rdoMinimalTrust.Location = new System.Drawing.Point(19, 92);
+            this.rdoMinimalTrust.Name = "rdoMinimalTrust";
+            this.rdoMinimalTrust.Size = new System.Drawing.Size(92, 17);
+            this.rdoMinimalTrust.TabIndex = 71;
+            this.rdoMinimalTrust.TabStop = true;
+            this.rdoMinimalTrust.Text = "Marginal Trust";
+            this.rdoMinimalTrust.UseVisualStyleBackColor = true;
+            this.rdoMinimalTrust.Click += new System.EventHandler(this.rdoMinimalTrust_Click);
+            // 
+            // rdoFullTrust
+            // 
+            this.rdoFullTrust.AutoSize = true;
+            this.rdoFullTrust.Location = new System.Drawing.Point(19, 128);
+            this.rdoFullTrust.Name = "rdoFullTrust";
+            this.rdoFullTrust.Size = new System.Drawing.Size(68, 17);
+            this.rdoFullTrust.TabIndex = 72;
+            this.rdoFullTrust.TabStop = true;
+            this.rdoFullTrust.Text = "Full Trust";
+            this.rdoFullTrust.UseVisualStyleBackColor = true;
+            this.rdoFullTrust.Click += new System.EventHandler(this.rdoFullTrust_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.rdoFullTrust);
+            this.panel1.Controls.Add(this.rdoMinimalTrust);
+            this.panel1.Controls.Add(this.rdoNoTrust);
+            this.panel1.Controls.Add(this.rdoKnownProblem);
+            this.panel1.Location = new System.Drawing.Point(23, 108);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(128, 160);
+            this.panel1.TabIndex = 73;
+            // 
+            // OwnerTrust
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnCancel;
+            this.ClientSize = new System.Drawing.Size(599, 361);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnSetTrustLevel);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.chkUltimateTrust);
+            this.Controls.Add(this.txtFingerprint);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.Name = "OwnerTrust";
+            this.Text = "OwnerTrust";
+            this.Load += new System.EventHandler(this.OwnerTrust_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtFingerprint;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox chkUltimateTrust;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Button btnSetTrustLevel;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.RadioButton rdoKnownProblem;
+        private System.Windows.Forms.RadioButton rdoNoTrust;
+        private System.Windows.Forms.RadioButton rdoMinimalTrust;
+        private System.Windows.Forms.RadioButton rdoFullTrust;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
---
+++ LynxPrivacy/LocalKeyStoreDialogs/OwnerTrust.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using LynxPrivacyLib;
+
+namespace LynxPrivacy.LocalKeyStoreDialogs
+{
+    public partial class OwnerTrust : Form
+    {
+        public OwnerTrust()
+        {
+            InitializeComponent();
+            label4.Text = "This is a known or suspected problem where communications from, or to, the email address are compromised or entirely untrustworthy.";
+            label5.Text = "This is the default for an unknown email address or someone you are just starting to communicate with.";
+            label6.Text = "";
+            label7.Text = "";
+            label8.Text = "";
+            label9.Visible = false;
+        }
+
+        private int m_keyId { get; set; }
+        private string m_keyType { get; set; }
+        internal void ConfigOwnerTrust(string keyType, int keyId)
+        {
+            m_keyType = keyType;
+            m_keyId = keyId;
+            if (keyType == "Public") {
+                rdoKnownProblem.Enabled = true;
+                rdoNoTrust.Enabled = true;
+                rdoMinimalTrust.Enabled = true;
+                rdoFullTrust.Enabled = true;
+                chkUltimateTrust.Enabled = false;
+            } else {
+                rdoKnownProblem.Enabled = false;
+                rdoNoTrust.Enabled = false;
+                rdoMinimalTrust.Enabled = false;
+                rdoFullTrust.Enabled = false;
+                chkUltimateTrust.Enabled = true;
+            }
+        }
+
+        private KeyStores m_key { get; set; }
+        private string m_trust { get; set; }
+        private void OwnerTrust_Load(object sender, EventArgs e)
+        {
+            Cursor.Current = Cursors.WaitCursor;
+            KeyStores key = Global.keyDb.KeyStores.Find(m_keyId);
+            m_key = key;
+            txtName.Text = key.KeyUsers.First().UserName;
+            txtEmail.Text = key.KeyUsers.First().Email;
+            StringBuilder buildFingerprint = new StringBuilder();
+            for (int x = 0; x < key.Fingerprint.Length; x++) {
+                buildFingerprint.Append(string.Format("{0} ", key.Fingerprint[x].ToString("X2")));
+            }
+            txtFingerprint.Text = buildFingerprint.ToString().TrimEnd();
+            m_trust = key.OwnerTrust;
+            switch (m_trust) {
+                case "ultimate":
+                    chkUltimateTrust.Checked = true;
+                    break;
+                case "full":
+                    rdoFullTrust.Checked = true;
+                    break;
+                case "marginal":
+                    rdoMinimalTrust.Checked = true;
+                    break;
+                case "unknown":
+                    rdoNoTrust.Checked = true;
+                    break;
+                case "never":
+                    rdoKnownProblem.Checked = true;
+                    break;
+                default:
+                    if (m_keyType == "Public")
+                        rdoNoTrust.Checked = true;
+                    break;
+            }
+            Cursor.Current = Cursors.Default;
+        }
+
+        private string m_newTrust { get; set; }
+        private void chkUltimateTrust_CheckedChanged(object sender, EventArgs e)
+        {
+            if (chkUltimateTrust.Checked)
+                m_newTrust = "ultimate";
+            else
+                m_newTrust = "";
+        }
+
+        private void btnSetTrustLevel_Click(object sender, EventArgs e)
+        {
+            m_key.OwnerTrust = m_newTrust;
+            Global.keyDb.SaveChanges();
+            label9.Text = "Owner Trust for the key updated successfully.";
+            label9.Visible = true;
+        }
+
+        private void rdoKnownProblem_Click(object sender, EventArgs e)
+        {
+            m_newTrust = "never";
+        }
+
+        private void rdoNoTrust_Click(object sender, EventArgs e)
+        {
+            m_newTrust = "unknown";
+        }
+
+        private void rdoMinimalTrust_Click(object sender, EventArgs e)
+        {
+            m_newTrust = "marginal";
+        }
+
+        private void rdoFullTrust_Click(object sender, EventArgs e)
+        {
+            m_newTrust = "full";
+        }
+        
+    }
+}
